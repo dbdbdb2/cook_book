e2016f54d9dcb72edeaa0dae038aea49461ee499
db <danielbernardograset@gmail.com>
2023-02-25 19:13:48 +0100

    Console Package

---
+++ lib/console/functions.dart
@@ -0,0 +1,20 @@
+
+class Car {
+  late int _speed;
+  int get speed => _speed;
+}
+
+void main() {
+
+  print ('Current speed -> ${currentSpeed(200)}');
+
+}
+
+int currentSpeed(int speed) => speed;
+
+// int breakk(int breakk) => currentSpeed(speed) - breakk;
+//
+// int speedUp(int speedUp) => currentSpeed(speed) + speedUp;
+
+
+
