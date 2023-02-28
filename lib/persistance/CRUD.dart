8c548b09d90568853e352a77a00a4c37d8adc736
db <danielbernardograset@gmail.com>
2023-02-28 13:22:18 +0100

    CRUD.dart

--- lib/persistance/CRUD.dart
+++ lib/persistance/CRUD.dart
@@ -23,103 +23,133 @@ void main() async {
         'CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, age INTEGER)',
       );
     },
+
     // version:x
     // This executes the onCreate function
     // Provides a path to perform database upgrades and downgrades
     version: 1,
   );
 
-  // Define a function that inserts dogs into the database
-  Future<void> insertDog(Dog dog) async {
+  // CRUD -> db.insert || db.query || db.update || db.delete
+  Future<void> createDog(Dog dog) async {
+
     // Get a reference to the database.
     final db = await database;
 
-    // Insert the Dog into the correct table. You might also specify the
-    // `conflictAlgorithm` to use in case the same dog is inserted twice.
-    //
-    // In this case, replace any previous data.
+    // Insert the Dog into the correct table.
+    // You might also specify the `conflictAlgorithm` in case the same dog is inserted twice.
     await db.insert(
       'dogs',
       dog.toMap(),
+
+      //Replace existing
       conflictAlgorithm: ConflictAlgorithm.replace,
     );
   }
 
-  // A method that retrieves all the dogs from the dogs table.
-  Future<List<Dog>> dogs() async {
-    // Get a reference to the database.
+  Future<List<Dog>> readAllDogs() async {
+
+    // Get a reference to the database
     final db = await database;
 
-    // Query the table for all The Dogs.
-    final List<Map<String, dynamic>> maps = await db.query('dogs');
+    // Query the table for getting data of all The Dogs
+    final List<Map<String, dynamic>> map = await db.query('dogs');
 
-    // Convert the List<Map<String, dynamic> into a List<Dog>.
-    return List.generate(maps.length, (i) {
-      return Dog(
-        id: maps[i]['id'],
-        name: maps[i]['name'],
-        age: maps[i]['age'],
-      );
-    });
+    // Convert the List<Map<String, dynamic> into a List<Dog>
+    return List.generate(map.length, (i) =>
+      Dog(
+        id: map[i]['id'],
+        name: map[i]['name'],
+        age: map[i]['age'],
+      )
+    );
   }
 
   Future<void> updateDog(Dog dog) async {
-    // Get a reference to the database.
+
+    // Get a reference to the database
     final db = await database;
 
-    // Update the given Dog.
+    // Update the given Dog
     await db.update(
       'dogs',
       dog.toMap(),
-      // Ensure that the Dog has a matching id.
+
+      // Ensure that the Dog has a matching id
       where: 'id = ?',
-      // Pass the Dog's id as a whereArg to prevent SQL injection.
+
+      // Pass the Dog's id as a whereArg to prevent SQL injection
       whereArgs: [dog.id],
+
     );
   }
 
   Future<void> deleteDog(int id) async {
-    // Get a reference to the database.
+
+    // Get a reference to the database
     final db = await database;
 
-    // Remove the Dog from the database.
+    // Remove the Dog from the database
     await db.delete(
       'dogs',
+
       // Use a `where` clause to delete a specific dog.
       where: 'id = ?',
+
       // Pass the Dog's id as a whereArg to prevent SQL injection.
       whereArgs: [id],
+
     );
+
   }
 
+
+  // C
+
   // Create a Dog and add it to the dogs table
-  var fido = const Dog(
+  Dog fido = /*const*/ Dog(
     id: 0,
     name: 'Fido',
     age: 35,
   );
 
-  await insertDog(fido);
+  await createDog(fido);
 
-  // Now, use the method above to retrieve all the dogs.
-  print(await dogs()); // Prints a list that include Fido.
 
-  // Update Fido's age and save it to the database.
+
+  // R
+
+  // print all before fido's update
+  print(await readAllDogs());
+
+
+
+
+  // U
+
+  // Update Fido's age and save it to the database
   fido = Dog(
     id: fido.id,
     name: fido.name,
     age: fido.age + 7,
   );
+
+  // print dogs before fido's update
   await updateDog(fido);
 
-  // Print the updated results.
-  print(await dogs()); // Prints Fido with age 42.
+  // Print the updated results
+  print(await readAllDogs());
+
+
+
+  // D
 
   // Delete Fido from the database.
   await deleteDog(fido.id);
 
   // Print the list of dogs (empty).
-  print(await dogs());
+  print(await readAllDogs());
+
 }
 
 class Dog {
