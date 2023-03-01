3d10dd1ded1bcf2254290be33f3bd6dd21238990
db <danielbernardograset@gmail.com>
2023-03-01 10:37:46 +0100

    StoreKeyValue > shared_preferences

--- lib/persistance/StoreKeyValue.dart
+++ lib/persistance/StoreKeyValue.dart
@@ -1,3 +1,85 @@
 
 //  shared_preferences
 
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MyHomePage(title: 'Shared preferences demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void initState() {
+    super.initState();
+    _loadCounter();
+  }
+
+  Future<void> _loadCounter() async {
+
+    // GET SHARED PREFERENCES
+    final preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      _counter = (preferences.getInt('counter') ?? 0);
+    });
+
+  }
+
+  // INCREMENTING COUNTER
+  Future<void> _incrementCounter() async {
+
+    final preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      _counter = (preferences.getInt('counter') ?? 0) + 1;
+      preferences.setInt('counter', _counter);
+    });
+
+  }
+
+  Widget build(BuildContext context) =>
+
+    Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title, style: TextStyle(fontSize: 35)),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+}
