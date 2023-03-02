3337dfd5734bb6e8da740d73e7dad205ed71039d
db <danielbernardograset@gmail.com>
2023-03-02 10:38:39 +0100

    PlayPauseVideo.dart

--- lib/plugins/PlayPauseVideo.dart
+++ lib/plugins/PlayPauseVideo.dart
@@ -8,3 +8,57 @@
 *     <uses-permission android:name="android.permission.INTERNET"/>
 *
 * */
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+
+    home: (
+      Video()
+    ),
+
+  );
+
+}
+
+class Video extends StatefulWidget {
+  const Video({Key? key}) : super(key: key);
+
+  State<Video> createState() => _VideoState();
+}
+
+class _VideoState extends State<Video> {
+
+  Widget build(BuildContext context) => MaterialApp(
+    debugShowCheckedModeBanner: false,
+
+    home: Scaffold(
+      backgroundColor: Colors.blue[200],
+
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Video', style: TextStyle(fontSize: 40)),
+        backgroundColor: Colors.blue[900],
+      ),
+
+      body: Container(
+        padding: const EdgeInsets.all(10),
+
+        child:  Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            color: Colors.red,
+          ),
+
+        ),
+      ),
+    ),
+  );
+}
