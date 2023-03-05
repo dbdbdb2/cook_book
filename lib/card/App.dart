9580c8ee73d5de3ec7f2338b7c88dd2f245dfeba
db <danielbernardograset@gmail.com>
2023-03-05 11:11:26 +0100

    cards package

---
+++ lib/card/App.dart
@@ -0,0 +1,13 @@
+import 'Cards.dart';
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+
+  Widget build(BuildContext context) {
+
+    return MaterialApp(
+      home: Cards(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
